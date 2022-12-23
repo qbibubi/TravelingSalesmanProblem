6f3ac4718a67ed563c38ac1411c69e82e7a7e4ae
qbibubi <jakub.sobieraj2137@gmail.com>
2022-12-23 13:29:43 +0100

    Change to layout. Starting to work on algorithm

--- app/src/main/java/com/example/travelingsalesmanproblem/MainActivity.kt
+++ app/src/main/java/com/example/travelingsalesmanproblem/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.travelingsalesmanproblem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var outcomeTV: TextView
+    private lateinit var startDestination: Spinner
+    private lateinit var finishDestination: Spinner
+    private lateinit var distanceInput: Number
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
